b073e1114c7ab17ddcbec9a40aef849fca8c17d8
QuanVo308 <minhquan.vo01@gmail.com>
2022-06-15 15:51:32 +0700

    rest django tutorial

--- backend3/backend3/urls.py
+++ backend3/backend3/urls.py
@@ -3,5 +3,10 @@ from django.urls import include, path
 
 urlpatterns = [
     path('test/', include('testapp.urls')),
+    path('api/', include('snippets.urls')),
     path('admin/', admin.site.urls),
+]
+
+urlpatterns += [
+    path('api-auth/', include('rest_framework.urls')),
 ]

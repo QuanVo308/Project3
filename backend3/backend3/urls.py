873492da714cdce409f37a01eade49ec161b3955
QuanVo308 <minhquan.vo01@gmail.com>
2022-06-16 16:58:28 +0700

    make urls api

--- backend3/backend3/urls.py
+++ backend3/backend3/urls.py
@@ -3,8 +3,9 @@ from django.urls import include, path
 
 urlpatterns = [
     path('test/', include('testapp.urls')),
-    path('api/', include('snippets.urls')),
+    path('testrest/', include('snippets.urls')),
     path('admin/', admin.site.urls),
+    path('api', include('api.urls'))
 ]
 
 urlpatterns += [

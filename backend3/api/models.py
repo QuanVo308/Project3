0f4589172599bfc300b0b9a682abe9dce1c08adb
duongngocbach <86738886+duongngocbach@users.noreply.github.com>
2022-06-20 17:38:33 +0700

    .

--- backend3/api/models.py
+++ backend3/api/models.py
@@ -42,6 +42,7 @@ class PopPlus(models.Model):
     def __str__(self) :
         return self.name
 
+
 class Pop(models.Model):
     name = models.CharField(max_length=20, unique=True)
     ring = models.CharField(max_length=20)

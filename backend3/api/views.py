b5e85b0c518bc1f14a951230f079cf21b2cae506
duongngocbach <86738886+duongngocbach@users.noreply.github.com>
2022-06-20 08:08:36 +0700

    Update views.py

--- backend3/api/views.py
+++ backend3/api/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from rest_framework import viewsets
 from rest_framework.response import Response
 from .models import *
-from .ultility import *
+from .utils import *
 from .serializers import *
 from .models import *
 

0ec0d3c1efd5ef1c30bd3eb96e5b98f031c22f5a
QuanVo308 <minhquan.vo01@gmail.com>
2022-06-22 16:23:44 +0700

    remove reload new tab

--- frontend3/src/components/Header/Header.js
+++ frontend3/src/components/Header/Header.js
@@ -1,6 +1,7 @@
 
 import {Nav} from 'react-bootstrap'
 import styles from './Header.module.scss'
+import { Routes, Route, Link, useNavigate, useLocation  } from "react-router-dom";
 
 export default function Header(){
     return(
@@ -8,13 +9,16 @@ export default function Header(){
             <div className={styles.title}>QUẢN LÝ TÀI NGUYÊN LOGIC INFMB FTEL</div>
             <Nav variant="tabs">
                 <Nav.Item>
-                    <Nav.Link href="/popplus">Pop+</Nav.Link>
+                    {/* <Nav.Link href="/popplus">Pop+</Nav.Link> */}
+                    <Link to="/popplus" className={styles.tabzxc} >Pop+</Link>
                 </Nav.Item>
                 <Nav.Item>
-                    <Nav.Link href="/pop">Pop</Nav.Link>
+                    {/* <Nav.Link href="/pop">Pop</Nav.Link> */}
+                    <Link to="/pop" className={styles.tabzxc}>Pop</Link>
                 </Nav.Item>
                 <Nav.Item>
-                    <Nav.Link href="/device">Device</Nav.Link>
+                    {/* <Nav.Link href="/device">Device</Nav.Link> */}
+                    <Link to="/device" className={styles.tabzxc}>Device</Link>
                 </Nav.Item>
             </Nav>
         </div>
